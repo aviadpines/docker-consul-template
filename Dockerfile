37799751710a266d195741267c6d64b570b86c65
apines <aviad.pines@intel.com>
2017-07-29 13:15:00 +0300

    fix volume permissions

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM hashicorp/consul-template:0.19.0-alpine
 
-RUN apk --update --no-cache add ca-certificates openssl
+RUN apk --update --no-cache add ca-certificates openssl tar
 
 ADD start.sh /usr/local/bin/start.sh
 RUN chmod +x /usr/local/bin/start.sh
+
+RUN mkdir -p /consul-template/output && \
+    chown -R consul-template:consul-template /consul-template
 
 VOLUME /consul-template/output
 
--- start.sh
+++ start.sh
@@ -5,8 +5,22 @@ if [ -z "$GIT_USER" ] || [ -z "$GIT_REPO" ] || [ -z "$GIT_PATH" ]; then
     exit 1
 fi
 
-echo "templates location: https://codeload.github.com/${GIT_USER}/${GIT_REPO}/tar.gz/master | tar -xz --strip=2 ${GIT_REPO}-master/${GIT_PATH}"
+echo "retrieving templates: https://codeload.github.com/${GIT_USER}/${GIT_REPO}/tar.gz/master | tar -xz --strip=2 ${GIT_REPO}-master/${GIT_PATH}"
+cd /consul-template/data && curl -s https://codeload.github.com/${GIT_USER}/${GIT_REPO}/tar.gz/master | tar -xz --strip=2 ${GIT_REPO}-master/${GIT_PATH}
 
-cd /consul-template/data && curl https://codeload.github.com/${GIT_USER}/${GIT_REPO}/tar.gz/master | tar -xz --strip=2 ${GIT_REPO}-master/${GIT_PATH}
+# concat all the templates
+TEMPLATES=$(find /consul-template/data/ -name "*.tpl" | \
+    sed -r 's|/consul-template/data/(.*).tpl|-template /consul-template/data/\1.tpl:/consul-template/output/\1.yml|g' | \
+    tr '\n' ' ')
+echo "templates string: $TEMPLATES"
 
-/bin/consul-template "$@"
+# If the data or config dirs are bind mounted then chown them.
+# Note: This checks for root ownership as that's the most common case.
+# (this is exactly what docker-entrypoint.sh does, and without it the
+# dir will be owned by root and we'll get permission denied)
+if [ "$(stat -c %u /consul-template/output)" != "$(id -u consul-template)" ]; then
+  chown consul-template:consul-template /consul-template/output
+fi
+
+echo "running execution command /bin/consul-template $@ $TEMPLATES"
+/usr/local/bin/docker-entrypoint.sh $TEMPLATES "$@"
